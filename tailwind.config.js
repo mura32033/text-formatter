56ab9845f3e6aa150a4290246fd16ce0704be406
mura32033 <mura3-2033@hotmail.co.jp>
2021-04-28 13:52:08 +0900

    Tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './*.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [],
+}
